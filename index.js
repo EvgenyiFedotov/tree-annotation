f5d3ca7e3a7f2941a0d84424cc09b7a7e32316b8
e.fedotov <fedotov1290@gmail.com>
2019-10-10 16:57:55 +0300

    build tree by types and ids

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 const recast = require("recast");
 const { readFileSync, writeFileSync } = require("fs");
+const chalkpipe = require("chalk-pipe");
+
+const { getNodesbyTypes, getNodesByIds } = require("./lib/file-tree");
 
 const buildReadme = cb => {
   const src = "./test/index.ts";
   const file = readFileSync(src, "utf8");
 
-  const fileTree = recast.parse(file, {
+  const parseResult = recast.parse(file, {
     parser: require("recast/parsers/typescript")
   });
 
-  buildContent(fileTree);
+  treeFormatting(parseResult);
 
   // writeFileSync("readme.md", buildContent(fileTree));
   if (cb) cb();
 };
 
-const buildContent = fileTree => {
-  const bodyByTypes = fileTree.program.body.reduce(...byType());
-  const { TSTypeAliasDeclaration, VariableDeclaration } = bodyByTypes;
+const treeFormatting = fileTree => {
+  const bodyByTypes = getNodesbyTypes(fileTree.program.body);
 
-  if (TSTypeAliasDeclaration) {
-    console.log("🎉  Types builded");
-  }
+  const bodyTypesByIds = Object.keys(bodyByTypes).reduce((memo, type) => {
+    const nodesByIds = getNodesByIds(type, bodyByTypes[type]);
+    if (Object.keys(nodesByIds).length) {
+      memo[type] = nodesByIds;
+      console.log(`🎉  ${chalkpipe("cyan")(type)} builded by ids`);
+    } else {
+      console.log(`👻  ${chalkpipe("yellow")(type)} skiped`);
+    }
+    return memo;
+  }, {});
 
-  if (VariableDeclaration) {
-    console.log("🎉  Variables builded");
-  }
+  console.log(Object.keys(bodyTypesByIds));
 };
 
 buildReadme();
