0496cd06e80f23f7edab749b42b09dfb2e403578
e.fedotov <fedotov1290@gmail.com>
2019-10-23 18:52:11 +0300

    add parse node with parameters for node with type 'interfaces'

--- test/interfaces.ts
+++ test/interfaces.ts
@@ -9,7 +9,6 @@ interface nametest3 {
     prop2: nametest2;
   };
 }
-
 interface nametest4 {
   prop1: 1;
   prop2: "1";
@@ -20,4 +19,15 @@ interface nametest4 {
     prop61: nametest1;
     prop62: nametest1 | nametest4;
   };
+}
+interface nametest5<T> {
+  [propName: string]: T;
+}
+interface nametest6<T = nametest1> {
+  prop1: T;
+  prop2: nametest2;
+}
+interface nametest7<T, K = nametest2> {
+  prop1: T;
+  prop2: K;
 }
