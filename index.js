f6976e0b2e9748de64132ff6ac6641f66f9ec69e
e.fedotov <fedotov1290@gmail.com>
2019-10-24 23:02:17 +0300

    parse nodes type 'functions'

--- index.js
+++ index.js
@@ -26,4 +26,5 @@ const getBlock = allNodesbyId => (func, nodes) =>
 // buildReadme("./test/export.ts");
 // buildReadme("./test/variables.ts");
 // buildReadme("./test/types.ts");
-buildReadme("./test/interfaces.ts");
+// buildReadme("./test/interfaces.ts");
+buildReadme("./test/functions.ts");
