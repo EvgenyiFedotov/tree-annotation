3b91d715d19ac724a36f6dd41a2d50105d25726b
e.fedotov <fedotov1290@gmail.com>
2019-11-12 16:46:13 +0300

    reset src and dependencies

--- test/__mocks-ts__/import.ts
+++
@@ -1,4 +0,0 @@
-import def from "./export";
-import { ExpConst1, ExpConst3 } from "./export";
-import { ExpFunc1 as testNameAs } from "./export";
-import * as EXP from "./export";
--- test/__mocks-ts__/namespaces.ts
+++
@@ -1,5 +0,0 @@
-namespace name1 {}
-namespace name2 {
-  const a = 1;
-  const b: string = "2";
-}
